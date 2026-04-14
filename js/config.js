777890c532bed8043d45ec06acd164c8a1c334e8
DESKTOP-883EC3D\soldesk <vjzm44@naver.com>
2026-04-14 15:17:03 +0900

    V3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_XIyR9WD0h', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1jekvok0vgv4kir5nu3ud3jbr7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6jhf6lqsrab0fmkr1mf4e2eggk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
